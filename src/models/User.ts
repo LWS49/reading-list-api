3c4c895fca71ca37679c574f0df96f0dfd22a56f
LWS49 <e0970244@u.nus.edu>
2025-01-22 17:17:45 +0800

    remove ! and add relationship to Book and ReadingProgress

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,24 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToMany } from "typeorm";
+import { ReadingProgress } from "./ReadingProgress";
+import { Book } from "./Book";
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
-    id!: number;
+    id: number;
 
     @Column({ unique: true })
-    email!: string;
+    email: string;
 
     @Column()
-    password!: string;
+    password: string;
 
     @CreateDateColumn()
-    createdAt!: Date;
+    createdAt: Date;
+
+    @OneToMany(() => Book, (book) => book.user)
+    books: Book[];
+
+    @OneToMany(() => ReadingProgress, (progress) => progress.user)
+    readingProgress: ReadingProgress[];
 }
