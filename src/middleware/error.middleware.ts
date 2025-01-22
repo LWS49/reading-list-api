72a4204b784d5b27823184f23cc300c48efa125a
LWS49 <e0970244@u.nus.edu>
2025-01-22 18:16:32 +0800

    add functionality to allow global error handler

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,14 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
+import { AppError } from '../utils/errors';
 
 export const errorHandler = (
-    err: Error,
+    err: Error | AppError,
     req: Request,
     res: Response,
-    next: NextFunction
+    next: NextFunction // though unused, all four parameters are necessary
+    // Express identifies error handling middleware by looking for functions with exactly four arguments
 ) => {
     console.error(err.stack);
+    if (err instanceof AppError) {
+        res.status(err.statusCode).json({
+            status: 'error', // include status for better clarity
+            message: err.message });
+        return;
+    }
+
+    // Handle other errors
     res.status(500).json({
+        status: 'error', // include status for better clarity
         message: 'Internal server error',
-        error: process.env.NODE_ENV === 'development' ? err.message : undefined
+        ...(process.env.NODE_ENV === 'development' && { error: err.message }) // only include error message in developement
     });
 };
