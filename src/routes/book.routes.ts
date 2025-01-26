f3685201bc4298635807808429730d9ef3014f6c
LWS49 <e0970244@u.nus.edu>
2025-01-26 21:29:34 +0800

    add listBooks, updateBook and deleteBook routes

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -8,8 +8,11 @@ const bookController = new BookController();
 
 router.use(authMiddleware); // Protect all book routes
 
+router.get('/list', (req, res, next) => bookController.listBooks(req, res, next));
 router.post('/', validateBookCreation, (req, res, next) => bookController.createBook(req, res, next));
 router.post('/:bookId/progress', (req, res, next) => bookController.updateProgress(req, res, next));
 router.get('/:bookId', (req, res, next) => bookController.getBookProgress(req, res, next));
+router.get('/:bookId/update', (req, res, next) => bookController.updateBook(req, res, next));
+router.delete('/:bookId/delete', (req, res, next) => bookController.deleteBook(req, res, next));
 
 export default router;
