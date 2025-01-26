7199e37d844f5dcf469d2a58e7fd0c6fe5b38ce3
LWS49 <e0970244@u.nus.edu>
2025-01-26 16:21:38 +0800

    coment out globalSetup and globalTeardown, unnecessary for now

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,14 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     coveragePathIgnorePatterns: ['/node_modules/'],
-    testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-    globalSetup: '<rootDir>/src/__tests__/setup.ts',
-    globalTeardown: '<rootDir>/src/__tests__/setup.ts',
+    testMatch: [
+        '**/__tests__/**/*.ts',       // Match test files
+        '**/?(*.)+(spec|test).ts',    // Match test files
+        '!**/__tests__/setup.ts',      // Exclude setup.ts from test matching
+        '!**/__tests__/globalSetup.ts', // Exclude globalSetup.ts from test matching
+        '!**/__tests__/globalTeardown.ts', // Exclude globalTeardown.ts from test matching
+    ],
+    // globalSetup: '<rootDir>/src/__tests__/globalSetup.ts',   // Correct path to globalSetup
+    // globalTeardown: '<rootDir>/src/__tests__/globalTeardown.ts', // Correct path to globalTeardown
+
 };
