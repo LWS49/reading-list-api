95419b46663a500950a162ac60e46595554b5563
lws49 <e0970244@u.nus.edu>
2025-08-27 11:19:40 +0800

    refactor: changed api/books to api/entries

--- frontend/src/app/api/entries/route.ts
+++ frontend/src/app/api/entries/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
-import { v4 as uuidv4 } from "uuid";
 import { Book } from "@/types/book";
+import { bookSchema } from "@/types/bookSchema";
+import { PrismaClient} from "@prisma/client";
 
 /*
 uuidv4() generates a unique ID.
@@ -40,8 +41,13 @@ It reads the JSON body of the request using await req.json().
 */
 
 export async function POST(req: Request) {
-  const body = await req.json();
-  const newEntry = { id: uuidv4(), status: "unread", ...body };
-  readingList.push(newEntry);
-  return NextResponse.json(newEntry, { status: 201 });
+  try {
+    const data = await req.json();
+    const parsed = bookSchema.parse(data);
+    readingList.push(parsed);
+    return NextResponse.json(parsed, { status: 201 });
+  } catch (e) {
+    console.error('Unexpected error:', e);
+    return NextResponse.json({ error: 'An unexpected error occurred.' }, { status: 400});
+  }
 }
